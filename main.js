055810da9110256934883996033198d02618b4a9
Apuroop Busetty <apu.busetty@gmail.com>
2020-04-29 01:23:15 +0530

    first version

---
+++ main.js
@@ -0,0 +1,75 @@
+console.log("hello");
+
+const colors = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF",
+];
+
+function getNewQuote() {
+  var quote;
+  fetch(
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+  )
+    .then((data) => data.json())
+    .then((data) => (quote = data))
+    .then(() => {
+      let random = Math.floor(Math.random() * 100 + 1);
+      let q = quote.quotes[random].quote;
+      let a = quote.quotes[random].author;
+      let length = colors.length;
+      let color = Math.floor(Math.random() * length - 1);
+
+      document.getElementById("text-content").innerHTML = q;
+      document.getElementById("author").innerHTML = "-" + " " + a;
+      document.getElementById("text-content").style.color = colors[color];
+      document.getElementById("wrap").style.backgroundColor = colors[color];
+    });
+}
